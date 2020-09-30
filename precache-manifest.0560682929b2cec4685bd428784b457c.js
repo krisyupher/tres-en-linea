7643f44be5a4e5397610a3f8b5d022ec0841077b
Cristian Florez <cflorez@quantumconnexion.com>
2020-09-30 10:17:13 -0500

    Updates

---
+++ precache-manifest.0560682929b2cec4685bd428784b457c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c7e7e41aefa33d62b55da55a8aa3255",
+    "url": "/tres-en-linea/index.html"
+  },
+  {
+    "revision": "d3d59ca03ef8d0c47d97",
+    "url": "/tres-en-linea/static/css/main.26a4d810.chunk.css"
+  },
+  {
+    "revision": "e61c675b3ee6ddc5992c",
+    "url": "/tres-en-linea/static/js/2.1e593aa9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tres-en-linea/static/js/2.1e593aa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3d59ca03ef8d0c47d97",
+    "url": "/tres-en-linea/static/js/main.9881dab7.chunk.js"
+  },
+  {
+    "revision": "23908618e59116fd3be8",
+    "url": "/tres-en-linea/static/js/runtime-main.d2b8d6cb.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/tres-en-linea/precache-manifest.0560682929b2cec4685bd428784b457c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tres-en-linea/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
